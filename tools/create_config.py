644f397a61cdd20e2d90eb6475992c51d971af8e
Hamlet <hamlets849@gmail.com>
2025-03-24 23:40:40 +0400

    .

--- tools/create_config.py
+++ tools/create_config.py
@@ -3,43 +3,41 @@ import sys
 
 import tomlkit
 
-config = {
-    "general": {
-        "debug": False,
-        "owners": [5161392463],
-    },
-    "database": {
-        "url": "your_db_url",
-        "name": "livebot",
-    },
-    "redis": {
-        "url": "your_redis_url",
-    },
-    "telegram": {
-        "token": "your_bot_token",
-        "log_chat_id": "",
-        "log_thread_id": 2,
-    },
-    "weather": {
-        "region": "",
-    },
-    "event": {
-        "start_time": "",
-        "end_time": "",
-        "open": False,
-    },
-    "channel": {
-        "id": "",
-        "chat_id": "",
-    },
+config = tomlkit.document()
+config["general"] = {
+    "debug": False,
+    "owners": [5161392463],
 }
-
+config["database"] = {
+    "url": "your_db_url",
+    "name": "livebot",
+}
+config["redis"] = {
+    "url": "your_redis_url",
+}
+config["telegram"] = {
+    "token": "your_bot_token",
+    "log_chat_id": "",
+    "log_thread_id": 2,
+}
+config["weather"] = {
+    "region": "",
+}
+config["event"] = {
+    "start_time": "",
+    "end_time": "",
+    "open": False,
+}
+config["channel"] = {
+    "id": "",
+    "chat_id": "",
+}
 
 if os.path.exists("config.toml"):
     print("Конфигурационный файл существует")
     sys.exit(1)
 
 with open("config.toml", "w") as f:
-    tomlkit.dump(config, f)
+    f.write(tomlkit.dumps(config))
 
 print("Конфигурационный файл успешно создан.")
