b7167ade0e74b0950843fb5bafe5c34b335e9307
OxM4LL0C <hamlets849@gmail.com>
2025-04-06 13:12:54 +0400

    .

--- src/consts.py
+++ src/consts.py
@@ -1,14 +1,6 @@
 from typing import Final
 
-from aiogram.types import InlineKeyboardButton
 from bson import ObjectId
-
-PAGER_CONTROLLERS: Final = [
-    InlineKeyboardButton(text="↩️", callback_data="{name} start {pos} {user_id}"),
-    InlineKeyboardButton(text="⬅️", callback_data="{name} back {pos} {user_id}"),
-    InlineKeyboardButton(text="➡️", callback_data="{name} next {pos} {user_id}"),
-    InlineKeyboardButton(text="↪️", callback_data="{name} end {pos} {user_id}"),
-]
 
 COIN_EMOJI: Final = "🪙"
 TELEGRAM_ID: Final = 777000
