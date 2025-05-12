02f31b38941a6a5c914460d54216b6c4745a08a1
0xM4LL0C <0xmll0c@proton.me>
2025-05-12 20:19:15 +0400

    .

--- src/helpers/filters.py
+++ src/helpers/filters.py
@@ -1,16 +1,11 @@
-from aiogram.enums import ChatType
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 
-class IsDigitFilter(BaseFilter):
-    async def __call__(self, message: Message) -> bool:
-        return message.text.isdigit()
-
-
-class ChatTypeFilter(BaseFilter):
-    def __init__(self, *chat_types: ChatType):
-        self.chat_types = chat_types
+class TextOrCommandFilter(BaseFilter):
+    def __init__(self, *aliases: str):
+        self.aliases = [a.lower() for a in aliases]
 
     async def __call__(self, message: Message) -> bool:
-        return any(message.chat.type == chat_type for chat_type in self.chat_types)
+        text = message.text.lower()
+        return text in self.aliases or (text.startswith("/") and text[1:] in self.aliases)
